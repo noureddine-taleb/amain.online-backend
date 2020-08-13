a29ac913a4a7baf23dc44354b3ded3ffc75db700
Nour-eddine Taleb <noureddinetaleb12@gmail.com>
2020-08-13 12:38:38 +0100

    🐛 FIX: promise go wild in reports cause serious damage in prod

--- src/controllers/Report.controllers.ts
+++ src/controllers/Report.controllers.ts
@@ -1,7 +1,6 @@
 import { Controller } from "./Controller";
 import { Request, Response } from "express";
 import { MPayment } from "../models/Payment.model";
-import { MBill } from "../models/Bill.model";
 import { MTreasury } from "../models/Treasury";
 import { MProject } from "../models/Project.model";
 
@@ -14,56 +13,55 @@ export class CReport implements Controller {
         .aggregate()
         .lookup({ from: 'bills', localField: 'billID', foreignField: '_id', as: 'billID'})
         .group({ _id: '$billID.projectID', earn: { $sum: '$amount' } })
-        console.log({reports})
         
-        // serialize data
+        // serialize data that is because any payment has exacly one bill
         for(const r of reports)
             r._id = r._id?.[0]
-        console.log({reports})
 
         // populate project id field
         await MProject.populate(reports, { path: '_id' })
-        console.log({reports})
 
         // add other data from treasury
         for(const r of reports){
-            r.earn |= 0
-            r.lost |= 0
-            console.log({projectID: r._id })
+            r.earn |= 0 // fallback to 0 if key is not set
+            r.lost |= 0 // fallback to 0 if key is not set
+
+            // calc earn
             r.earn += (await MTreasury.aggregate()
             .match({ amount : { $gte: 0 } , projectID: r._id._id })
             .group({ _id: null, earn: { $sum: '$amount' } }))[0]?.earn || 0
 
+            // calc lost
             r.lost += (await MTreasury.aggregate()
             .match({ amount : { $lt: 0 } , projectID: r._id._id })
             .group({ _id: null, lost: { $sum: '$amount' } }))[0]?.lost || 0
 
+            // we need only name to save bandwidth
             r.project = r._id.name
             delete r._id
         }
-        console.log({reports})
 
         // 'other' transactions
         reports.push({
             project: '<other>',
-
+            
+            // calc earn
             earn : (await MTreasury.aggregate()
             .match({ amount : { $gte: 0 } , name: '<other>' })
             .group({ _id: null, earn: { $sum: '$amount' } }))[0]?.earn || 0,
-
+            
+            // calc lost
             lost : (await MTreasury.aggregate()
             .match({ amount : { $lt: 0 } , name: '<other>' })
             .group({ _id: null, lost: { $sum: '$amount' } }))[0]?.lost || 0
         })
-        console.log({reports})
-        
-        // calculate balance
+
+        // calc balance
         let total = 0;
         for(const r of reports){
             r.balance = r.earn + r.lost
             total += r.balance
         }
-        console.log({reports})
 
         return res.json({ reports, total })
     }
