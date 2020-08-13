ec737a44d75b2f3a8d14498adfd8343b9f242344
Nour-eddine Taleb <noureddinetaleb12@gmail.com>
2020-08-13 12:21:14 +0100

    🐛 FIX: instrument some func calls to fix bugs

--- src/controllers/Report.controllers.ts
+++ src/controllers/Report.controllers.ts
@@ -15,30 +15,34 @@ export class CReport implements Controller {
         .lookup({ from: 'bills', localField: 'billID', foreignField: '_id', as: 'billID'})
         .group({ _id: '$billID.projectID', earn: { $sum: '$amount' } })
         console.log({reports})
+        
         // serialize data
         for(const r of reports)
             r._id = r._id?.[0]
         console.log({reports})
+
         // populate project id field
-        MProject.populate(reports, { path: '_id' })
+        await MProject.populate(reports, { path: '_id' })
         console.log({reports})
+
         // add other data from treasury
         for(const r of reports){
             r.earn |= 0
             r.lost |= 0
             console.log({projectID: r._id })
             r.earn += (await MTreasury.aggregate()
-            .match({ amount : { $gte: 0 } , projectID: r._id })
+            .match({ amount : { $gte: 0 } , projectID: r._id._id })
             .group({ _id: null, earn: { $sum: '$amount' } }))[0]?.earn || 0
 
             r.lost += (await MTreasury.aggregate()
-            .match({ amount : { $lt: 0 } , projectID: r._id })
+            .match({ amount : { $lt: 0 } , projectID: r._id._id })
             .group({ _id: null, lost: { $sum: '$amount' } }))[0]?.lost || 0
 
             r.project = r._id.name
             delete r._id
         }
         console.log({reports})
+
         // 'other' transactions
         reports.push({
             project: '<other>',
@@ -52,6 +56,7 @@ export class CReport implements Controller {
             .group({ _id: null, lost: { $sum: '$amount' } }))[0]?.lost || 0
         })
         console.log({reports})
+        
         // calculate balance
         let total = 0;
         for(const r of reports){
@@ -59,6 +64,7 @@ export class CReport implements Controller {
             total += r.balance
         }
         console.log({reports})
+
         return res.json({ reports, total })
     }
 
